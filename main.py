d3af6e56084b8c22c127506e541802df9fdb4175
TokyoGaijin <mbfleish1105@live.com>
2023-03-10 23:02:29 +0900

    cant display maze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import player as p
 import colorswatch as cs
+import maze
 from enum import Enum
 
 
@@ -19,27 +20,37 @@ FPS = 60
 inPlay = True
 player = p.Player(WINDOW, SCREEN_X / 2, SCREEN_Y / 2)
 
+def init():
+    maze.build(WINDOW)
+    print(len(maze.current_maze))
+
 def update():
     player.update()
 
 def draw():
     player.draw()
+    maze.draw()
 
 
+def main():
+    global inPlay
+    init()
 
+    while inPlay:
+        CLOCK.tick(FPS)
 
-while inPlay:
-    CLOCK.tick(FPS)
-
-    key = pygame.key.get_pressed()
-    if key[pygame.K_ESCAPE]:
-        inPlay = False
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        key = pygame.key.get_pressed()
+        if key[pygame.K_ESCAPE]:
             inPlay = False
 
-    update()
-    draw()
-    pygame.display.update()
-    WINDOW.fill(BG)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                inPlay = False
+
+        update()
+        draw()
+        pygame.display.update()
+        WINDOW.fill(BG)
+
+if __name__ == "__main__":
+    main()
